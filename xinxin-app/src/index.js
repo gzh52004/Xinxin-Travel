cc84748140aca8ece51eef412ad09cde85e75ac2
BruceWa1ne <52929264+BruceWa1ne@users.noreply.github.com>
2020-10-27 17:38:58 +0800

    10.27 完成底部导航栏路由切换

---
+++ xinxin-app/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, BrowserRouter } from "react-router-dom";
+import "./index.scss";
+import App from "./App.jsx";
+import reportWebVitals from "./reportWebVitals";
+import "antd-mobile/dist/antd-mobile.css";
+
+import "./utils/rem";
+
+// 根据环境不同切换不同的路由模式
+// process.env.NODE_ENV： development, production
+const Router =
+  process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ xinxin-app/src/pages/Reg/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Reg = () => {
+  return <div>注册</div>;
+};
+
+export default Reg;
